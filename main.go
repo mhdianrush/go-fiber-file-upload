cbafcd79290de62f845d15db16041f86aaf8b0c3
mhdianrush <mhdianrush@gmail.com>
2023-07-15 17:16:47 +0700

    Developed for Route, Server and Template HTML engine

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", nil)
+		// the second parameter is data send to view
+	})
+	
+
+	app.Listen(":8080")
+}
